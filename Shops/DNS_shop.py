f67c7f075276d0f1906ce6eabd5253f5ae024780
Демидов Ярослав Романович <i.demidov@fintech.ru>
2025-06-10 17:12:58 +0300

    sss

--- Shops/DNS_shop.py
+++ Shops/DNS_shop.py
@@ -1,6 +1,8 @@
 import time
 import sys
 import re
+from warnings import catch_warnings
+
 import psycopg
 import selenium
 from fake_useragent import UserAgent
@@ -22,17 +24,17 @@ options.add_argument("--disable-extensions")
 options.add_experimental_option('useAutomationExtension', False)
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.binary_location = 'D:\Chrome_with_Driver\chrome-win64\chrome.exe'
-# options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 
+service = Service(executable_path="D:\Chrome_with_Driver\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=service, options=options)
+driver.implicitly_wait(10)
+actions = ActionChains(driver)
 ua = UserAgent()
 
 def open_dns():
     userAgent = ua.random
     options.add_argument(f'user-agent={userAgent}')
-    service = Service(executable_path="D:\Chrome_with_Driver\chromedriver-win64\chromedriver.exe")
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.implicitly_wait(10)
-    actions = ActionChains(driver)
     driver.get(path_to_dns)
     time.sleep(3)
     xpath_to_button = '/html/body/a'
@@ -59,12 +61,35 @@ def open_dns():
     else:
         return False
 
-# '<a target="_blank" rel="noopener noreferrer" href="https://www.dns-shop.ru/">DNS</a>'
-# xpath_to_body = '/html/body/a'
-# body = driver.find_element(By.XPATH, xpath_to_body)
-# # print(body.get_attribute('innerHTML'))
-# body.send_keys(Keys.END)
 result_code = True
-
-while True:
+while result_code:
     result_code = open_dns()
+
+# В конец страницы
+xpath_to_body = '/html/body'
+body = driver.find_element(By.XPATH, xpath_to_body)
+body.send_keys(Keys.END)
+
+i = 0
+res = True
+path_to_button = f''
+
+while i < 100:
+    path_to_button = f'/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[{i}]/div/button'
+    i += 1
+    try:
+        body = driver.find_element(By.XPATH, path_to_button)
+        print(body.get_attribute('innerHTML'))
+        actions.move_to_element(body).click().perform()
+    except:
+        print('Не угадали')
+print('Страницы закончились')
+# while True:
+#     pass
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div/button
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[5]/div/button
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[7]/div/button
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[9]/div/button
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[13]/div/button
+
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div/button
--- Teatae.py
+++ Teatae.py
@@ -1,3 +1,96 @@
-import openpyxl
+import time
+import sys
+import re
+from warnings import catch_warnings
 
-book = openpyxl.load_workbook('/Users/yarik/Downloads/TestExcelFile.xlsx')
+import psycopg
+import selenium
+from fake_useragent import UserAgent
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver import ChromeOptions
+from logger import logging
+
+path_to_dns = "http://127.0.0.1:5000/link"
+
+options = ChromeOptions()
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument("--disable-extensions")
+options.add_experimental_option('useAutomationExtension', False)
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.binary_location = 'D:\Chrome_with_Driver\chrome-win64\chrome.exe'
+# options.add_argument("--headless=new")
+
+service = Service(executable_path="D:\Chrome_with_Driver\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=service, options=options)
+driver.implicitly_wait(10)
+actions = ActionChains(driver)
+ua = UserAgent()
+
+def open_dns():
+    userAgent = ua.random
+    # print(userAgent)
+    options.add_argument(f'user-agent={userAgent}')
+    driver.get(path_to_dns)
+    time.sleep(3)
+    xpath_to_button = '/html/body/a'
+    button = driver.find_element(By.XPATH, xpath_to_button)
+    actions.move_to_element(button).click().perform()
+
+    time.sleep(3)
+    driver.switch_to.window(driver.window_handles[1])
+
+    path_to_403 = '/html/body'
+    head = driver.find_element(By.XPATH, path_to_403)
+    result = head.get_attribute('innerHTML')
+    # print(result)
+
+    x = re.findall("(Доступ к сайту www.dns-shop.ru запрещен.)", result)
+    # print(len(x))
+
+    if len(x) != 0:
+        time.sleep(3)
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+        driver.close()
+        return True
+    else:
+        return False
+
+result_code = True
+while result_code:
+    result_code = open_dns()
+
+# В конец страницы
+xpath_to_body = '/html/body'
+body = driver.find_element(By.XPATH, xpath_to_body)
+body.send_keys(Keys.END)
+
+i = 0
+res = True
+path_to_button = f''
+
+while i < 100:
+    path_to_button = f'/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[{i}]/div/button'
+    i += 1
+    try:
+        body = driver.find_element(By.XPATH, path_to_button)
+        print(body.get_attribute('innerHTML'))
+        actions.move_to_element(body).click().perform()
+    except:
+        print('Не угадали')
+print('Страницы закончились')
+# while True:
+#     pass
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div/button
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[5]/div/button
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[7]/div/button
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[9]/div/button
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[13]/div/button
+
+#/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div/button
